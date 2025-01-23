c1af7b5fa0421c899ab0f48cab59e88a2ffada4c
sfidan42 <sadettinfidan00@gmail.com>
2025-01-23 22:22:51 +0300

    done first 5 exercises

--- sources/QuantumSimulator.py
+++
@@ -1,9 +0,0 @@
-from qiskit_aer import Aer
-from qiskit import transpile
-
-def qsim(circuit):
-    backend = Aer.get_backend('qasm_simulator')  # Get the Aer simulator
-    circuit = transpile(circuit, backend)  # Optimize and transpile
-    result = backend.run(circuit, shots = 500).result()  # 500 shots
-    counts = result.get_counts(circuit)
-    return counts
